06c95d558a19df7141dc852a21b4518d8396afc7
Emanuel Martinez <me@manumartinez.es>
2021-12-12 16:36:35 +0100

    Create action to take an account by ID

--- src/models/types/data/account.ts
+++ src/models/types/data/account.ts
@@ -1,20 +1,20 @@
 import { Account, _tableName } from "../Account";
+import { db } from "../../../http/core/db";
 
-export const findById = (id: number, callback: Function) => {
+export const findById = (accountID: number, callback: Function) => {
 
-    const queryString = `
-        SELECT 
-            *
-        FROM 
-            ${_tableName}
-        WHERE
-            id="${id}"
-    `;
-
-    // make server call
-
-    // prepare response json
-
-    // call callback
+    const queryString = `SELECT * FROM ${_tableName} WHERE id=?`;
+    db.query(queryString, [accountID], (err, result) => {
+        if (err) { callback(err) }
+        const row = result[0];
+        const account: Account = {
+            id: accountID,
+            name: row.name,
+            username: row.username,
+            email: row.email,
+            phone: row.phone
+        };
+        callback(null, account);
+    });
 
 };
