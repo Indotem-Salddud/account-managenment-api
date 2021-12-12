d997738d81e9d9344810495029ab1920d3d0bb1e
Emanuel Martinez <me@manumartinez.es>
2021-12-12 14:06:11 +0100

    Include swagger for documentation

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,24 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import { apiDocumentation } from './docs/api-docs.js'
 
 require('dotenv').config();
 
 const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // getting ip purpouse
 app.set("trust proxy", true); 
 app.set("PORT", process.env.PORT || 3000);
+// setting documentation
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 
 app.listen(app.get("PORT"), () => {
 console.log(`Starting app at: ${app.get("PORT")}`)
