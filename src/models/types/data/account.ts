82e4178e5e39070a484195b04137ecd56c52bd69
Emanuel Martinez <me@manumartinez.es>
2021-12-12 16:38:38 +0100

    Added more documentation

--- src/models/types/data/account.ts
+++ src/models/types/data/account.ts
@@ -1,6 +1,12 @@
 import { Account, _tableName } from "../Account";
 import { db } from "../../../http/core/db";
 
+/**
+ * ! Get account data by account ID
+ * * whitehatdevv - 2021/12/12
+ * @param accountID {Number}
+ * @param callback {Function}
+ */
 export const findById = (accountID: number, callback: Function) => {
 
     const queryString = `SELECT * FROM ${_tableName} WHERE id=?`;
@@ -16,5 +22,15 @@ export const findById = (accountID: number, callback: Function) => {
         };
         callback(null, account);
     });
+
+};
+
+/**
+ * ! Get all accounts data storage in DB
+ * * whitehatdevv - 2021/12/12
+ * @param callback {Function}
+ */
+export const findAll = (callback: Function) => {
+
 
 };
