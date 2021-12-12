333cdc807fe7b9ffafffeff3fa8486daa2f5c38f
Emanuel Martinez <me@manumartinez.es>
2021-12-12 13:37:35 +0100

    Init basic infraestructure

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res: express.Response) => {
+  res.send('The sedulous hyena ate the antelope!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Starting aplication at "+process.env.PORT);
+});
