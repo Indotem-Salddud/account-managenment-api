71af81add6d6efb178f44818c039d6607c19a24c
Emanuel Martinez <me@manumartinez.es>
2021-12-13 19:30:24 +0100

    Verified token

--- src/http/middlewares/JWTMiddelware.ts
+++ src/http/middlewares/JWTMiddelware.ts
@@ -1,9 +1,22 @@
-import * as jsonwebtoken from 'jsonwebtoken';
+import express from 'express';
+import * as jwt from 'jsonwebtoken';
+import {_handleResponse} from '../common/common';
 
 export module JWTMiddelware {
-    
-    export const _validateToken = () => {
+  export const _verify = (req: express.Request, res: express.Response) => {
+    const token: string = req.headers.authorization;
+    // check token
+    if (!token) {
+      _handleResponse(
+        {
+          statusCode: 401,
+          message: 'Authorization header is not received',
+        },
+        res
+      );
+    }
 
-    };
+    jwt.verify
 
-};
+  };
+}
