c3b70cc1751ae7b3268346e6ac1aea9a92ed6a84
Emanuel Martinez <me@manumartinez.es>
2021-12-13 20:23:01 +0100

    Debugger standard methods

--- src/http/common/Debug/Debugger.ts
+++ src/http/common/Debug/Debugger.ts
@@ -2,4 +2,17 @@
  * ! Module debugger to show logs in cosole
  * * whitehatdevv - 2021/12/13
  */
-export module Debuuger {}
+export module Debuuger {
+  /**
+   * ! Debugger to show log in console
+   * * whitehatdevv - 2021/12/13
+   * @param environment {DebbugerEnvironments}
+   * @param type {DebuggerTypes}
+   * @param log {string}
+   */
+  export const _printLog = (
+    environment: DebbugerEnvironments,
+    type: DebuggerTypes,
+    log: string
+  ) => {};
+}
