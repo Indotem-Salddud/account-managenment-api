a537429b72505947b26e502981b28431e3bb8603
Emanuel Martinez <me@manumartinez.es>
2021-12-13 20:09:51 +0100

    Created token payload and implement actor to decode data

--- src/http/middlewares/JWTMiddelware.ts
+++ src/http/middlewares/JWTMiddelware.ts
@@ -1,32 +1,63 @@
 import express from 'express';
 import * as jwt from 'jsonwebtoken';
-import { Token } from '../../models/types/Token';
+import {Token} from '../../models/types/Token';
 import {_handleResponse} from '../common/common';
+import {TokenPayload} from '../../models/types/gen/tokenPayload';
 
 export module JWTMiddelware {
 
     /**
-     * ! Main actor to sign a JWT Token
+     * ! Main actor to decode the token and get payload
      * * whitehatdevv - 2021/12/13
-     * * Token valid for 2 hours 
-     * TODO: Pending validation of token expiration
-     * @param accountID {string}
-     * @param role {string}
-     * @return token {string}
+     * @param req {Request}
+     * @param res {Response}
+     * @returns TokenPayload
      */
-    export const _signIn = (accountID: string, role: string) : Token => {
-        return {
-            expiration: Math.floor(Date.now()/1000) + (60*60*2),
-            token: jwt.sign({
-                exp: Math.floor(Date.now()/1000) + (60*60*2),
-                payload: {
-                    accountID: accountID,
-                    role: role
-                }
-            }, process.env.JWT_PRIVATE_KEY),
-            type: "Bearer"
-        };
+  export const _decode = (req: express.Request, res: express.Response) : TokenPayload => {
+    const token: string = req.headers.authorization.split(' ')[1];
+    // check token
+    if (!token) {
+      _handleResponse(
+        {
+          statusCode: 401,
+          message: 'Authorization header is not received',
+        },
+        res
+      );
     }
+    // get payload
+    const payload = jwt.decode(token, {complete: true});
+    return {
+        accountID: payload.payload.accountID,
+        role: payload.payload.role
+    }
+  };
+
+  /**
+   * ! Main actor to sign a JWT Token
+   * * whitehatdevv - 2021/12/13
+   * * Token valid for 2 hours
+   * TODO: Pending validation of token expiration
+   * @param accountID {string}
+   * @param role {string}
+   * @return token {string}
+   */
+  export const _signIn = (accountID: string, role: string): Token => {
+    return {
+      expiration: Math.floor(Date.now() / 1000) + 60 * 60 * 2,
+      token: jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 2,
+          payload: {
+            accountID: accountID,
+            role: role,
+          },
+        },
+        process.env.JWT_PRIVATE_KEY
+      ),
+      type: 'Bearer',
+    };
+  };
 
   /**
    * ! Main actor to verify a token for granted access
@@ -54,11 +85,11 @@ export module JWTMiddelware {
 
     // validate token
     jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, value) => {
-      if (err) {
+      if (err || Date.now()/1000 > value.exp) {
         _handleResponse(
           {
             statusCode: 403,
-            message: 'Error validating the token',
+            message: 'Token provided is not valid',
           },
           res
         );
---
+++ src/models/types/gen/tokenPayload.ts
@@ -0,0 +1,4 @@
+export interface TokenPayload {
+  accountID: string;
+  role: string;
+}
