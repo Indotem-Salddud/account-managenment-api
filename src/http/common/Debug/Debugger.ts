bc7ee190ad7779d63ae26870f1bb9157a7ab04a5
Emanuel Martinez <me@manumartinez.es>
2021-12-13 20:21:17 +0100

    Create debugger env and types

--- src/http/common/Debug/Debugger.ts
+++ src/http/common/Debug/Debugger.ts
@@ -2,8 +2,4 @@
  * ! Module debugger to show logs in cosole
  * * whitehatdevv - 2021/12/13
  */
-export module Debuuger {
-
-    
-
-}
+export module Debuuger {}
---
+++ src/http/common/Debug/Environments.ts
@@ -0,0 +1,9 @@
+/**
+ * ! Environments to control debugger
+ * * whitehatdevv - 2021/12/13
+ */
+enum DebbugerEnvironments {
+  production, 
+  testing,
+  debugger,
+}
