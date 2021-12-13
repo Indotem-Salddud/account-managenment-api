413152eab45b7d5118b3157007bc9f6a1eabcd00
Emanuel Martinez <me@manumartinez.es>
2021-12-13 20:33:13 +0100

    Finished complete debugger to acomplish different environments

--- src/http/common/Debug/Debugger.ts
+++ src/http/common/Debug/Debugger.ts
@@ -1,4 +1,4 @@
-import { DebuggerProtocol } from "./DebuggerProtocol";
+import {DebuggerProtocol} from './DebuggerProtocol';
 
 /**
  * ! Module debugger to show logs in cosole
@@ -16,5 +16,39 @@ export class Debuuger implements DebuggerProtocol {
     environment: DebbugerEnvironments,
     type: DebuggerTypes,
     log: string
-  ) => {};
+  ) => {
+    switch (environment) {
+      case DebbugerEnvironments.debugger:
+        this._manageLogType(type, log);
+        break;
+      case DebbugerEnvironments.production:
+        console.log(
+          '----- PRODUCTION: Log cannot be printed due to protection level'
+        );
+        break;
+      case DebbugerEnvironments.testing:
+        this._manageLogType(type, log);
+        break;
+    }
+  };
+
+  /**
+   * ! Main actor to manage debugger types
+   * * whitehatdevv - 2021/12/13
+   * @param type {DebuggerTypes}
+   * @param log {string}
+   */
+  _manageLogType(type: DebuggerTypes, log: string) {
+    switch (type) {
+      case DebuggerTypes.alert:
+        console.warn(`----- WARNING: ${log}`);
+        break;
+      case DebuggerTypes.debug:
+        console.log(`----- DEBUG: ${log}`);
+        break;
+      case DebuggerTypes.alert:
+        console.error(`----- ALERT: ${log}`);
+        break;
+    }
+  }
 }
--- src/http/common/Debug/DebuggerProtocol.ts
+++ src/http/common/Debug/DebuggerProtocol.ts
@@ -8,4 +8,8 @@ export interface DebuggerProtocol {
     type: DebuggerTypes,
     log: string
   ): any;
+  _manageLogType(
+    type: DebuggerTypes, 
+    log: string
+  ): any;
 }
