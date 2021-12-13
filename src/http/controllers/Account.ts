890a86c66e6d65a6fc8ae2a040f9cb7eff93b219
Emanuel Martinez <me@manumartinez.es>
2021-12-13 14:38:19 +0100

    Create main architecture to configurate routes and controller for accounts

---
+++ src/http/controllers/Account.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export module AccountsController {
+
+    /**
+     * ! Controller function to get account by their ID
+     * * whitehatdevv - 2021/12/13
+     * @param req {Request}
+     * @param res {Response}
+     */
+    export const _getById = async(req: express.Request, res: express.Response) => {
+
+
+    };
+
+};
---
+++ src/routes/Accounts.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { AccountsController } from '../http/controllers/Account';
+
+export const AccountsRoute = (app: express.Application) => {
+
+    /**
+     * * Get account by ID endpoint 
+     * @param acccountID {Number}
+     * @protected Admin or User with accountID
+     */
+    app.get(`/:accountID`, [], AccountsController._getById);
+    
+
+};
