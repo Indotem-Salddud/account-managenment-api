9fb62b4e0135dd87cafb661717996566e74a1d7a
Emanuel Martinez <me@manumartinez.es>
2021-12-13 18:22:54 +0100

    Added direction field

--- src/models/sql/Account.sql
+++ src/models/sql/Account.sql
@@ -6,6 +6,7 @@ CREATE TABLE `Accounts` (
     username VARCHAR(200) NOT NULL UNIQUE KEY,
     email VARCHAR(150) NOT NULL UNIQUE KEY,
     phone VARCHAR(20) NOT NULL,
+    direction VARCHAR(255) NOT NULL COMMENT 'It should be a direction used JSON Format file',  
     password VARCHAR(255) NOT NULL,
     role TINYINT(1) NOT NULL DEFAULT 1 COMMENT '1 - Customer(Dependent) / 2 - Customer(Manager)',
     status TINYINT(1) DEFAULT 1 NOT NULL COMMENT '0 - Inactive / 1 - Active',
