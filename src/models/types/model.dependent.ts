736a21500f41586fb1230de0b525d285b459fdf4
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2021-12-19 09:00:13 +0100

    Fix constant name typo

--- src/models/types/model.dependent.ts
+++ src/models/types/model.dependent.ts
@@ -1,7 +1,7 @@
 import {Identificable} from './gen/gen.identificable';
 import {Direction} from './gen/gen.direction';
 
-export const _depdendentTableName = 'Dependents';
+export const _dependentTableName = 'Dependents';
 export interface Dependent extends Identificable {
   name: string;
   phone: string;
