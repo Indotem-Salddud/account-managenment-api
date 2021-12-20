96029b9d660bad54ad998cf990e69a6ccdfacbe5
Emanuel Martinez <me@manumartinez.es>
2021-12-20 15:08:52 +0100

    Refactor function

--- src/http/middlewares/middelware.access.validation.ts
+++ src/http/middlewares/middelware.access.validation.ts
@@ -5,10 +5,10 @@ import { _handleResponse } from '../common/common.responseHandler';
 
 export module AccessControlMiddelware {
 
-    export const _grantAccess = (resource: string, f: (query: Query, resource: string) => Permission) => {
+    export const _grantAccess = (f: (query: Query) => Permission) => {
         return (req: express.Request, res: express.Response, next: express.NextFunction) => {
             // make a lambda function to run a validation
-            const permission = f(ac.can(req.user.role), resource);
+            const permission = f(ac.can(req.user.role));
             if (!permission.granted) {
                 _handleResponse(
                     {statusCode: 500, message: 'Server response cannot be processed'},
