5cb523bbce01ada28395e494993f476a647550c3
Emanuel Martinez <me@manumartinez.es>
2021-12-21 16:35:03 +0100

    Set getter method and init data staff

--- src/http/core/build/core.build.sql.ts
+++ src/http/core/build/core.build.sql.ts
@@ -1,6 +1,5 @@
 interface SQLQueryConstructorMethods {
-  findAll(get?: [string]): SQLConstructorMiddleType;
-  findBy(where: object, get?: [string]): SQLConstructorMiddleType;
+  findBy(get?: [string], where?: object): SQLConstructorMiddleType;
   save<T>(val: T): SQLConstructorMiddleType;
   update<T>(where?: object, data?: T): SQLConstructorMiddleType;
   deleteAll(): SQLConstructorMiddleType;
@@ -10,16 +9,16 @@ interface SQLQueryConstructorMethods {
 type SQLConstructorMiddleType = () => string;
 
 const _basicSQLCodes = {
-    table: '@table',
-    columns: '@column',
-    condition: '@condition'
+  table: '@table',
+  columns: '@column',
+  condition: '@condition',
 };
 
 const _basicSQLStructure = {
-    select: 'SELECT @columns FROM @table WHERE @condition',
-    insert: 'INSERT INTO @table VALUES @columns',
-    update: 'UPDATE @table SET @columns',
-    delete: 'DELETE FROM @table WHERE @condition'
+  select: 'SELECT @columns FROM @table WHERE @condition',
+  insert: 'INSERT INTO @table VALUES @columns',
+  update: 'UPDATE @table SET @columns',
+  delete: 'DELETE FROM @table WHERE @condition',
 };
 
 /**
@@ -27,44 +26,52 @@ const _basicSQLStructure = {
  * * whitehatdevv - 2021/12/21
  */
 class SQLQueryConstructor implements SQLQueryConstructorMethods {
-
   // * Singleton
   static shared = new SQLQueryConstructor();
 
-  // * Methods
-  findAll(get?: [string]): SQLConstructorMiddleType {
-    return () => {
-        return "";
-    }
+  // * Private methods
+  private _getter(base: string, get?: [string]) {
+    base.replace(
+      _basicSQLCodes.columns,
+      !get
+        ? get.reduce((acc = '', val) =>
+            acc.length == 0 ? `${val}` : `${acc}, ${val}`
+          )
+        : '*'
+    );
   }
 
-  findBy(where: object, get?: [string]): SQLConstructorMiddleType {
+  // * Methods
+  findBy(get?: [string], where?: object): SQLConstructorMiddleType {
     return () => {
-        return "";
-    }
+      let basic = _basicSQLStructure.select;
+      // set getter
+      this._getter(basic, get);
+      return basic;
+    };
   }
 
   save<T>(val: T): SQLConstructorMiddleType {
     return () => {
-        return "";
-    }
+      return '';
+    };
   }
 
   update<T>(where?: object, data?: T): SQLConstructorMiddleType {
     return () => {
-        return "";
-    }
+      return '';
+    };
   }
 
   deleteAll(): SQLConstructorMiddleType {
     return () => {
-        return "";
-    }
+      return '';
+    };
   }
 
   delete(where?: object): SQLConstructorMiddleType {
     return () => {
-        return "";
-    }
+      return '';
+    };
   }
 }
