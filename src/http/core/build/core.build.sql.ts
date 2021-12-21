37e65cc0a2fb80cccc66713b5f1c55cf8e1b1bc9
Emanuel Martinez <me@manumartinez.es>
2021-12-21 14:12:08 +0100

    Implemented all methods in query builder

--- src/http/core/build/core.build.sql.ts
+++ src/http/core/build/core.build.sql.ts
@@ -1,4 +1,5 @@
 interface SQLQueryConstructorMethods {
+  _tableName: string;
   findAll(get?: [string]): string;
   findBy(get?: [string], where?: object): string;
   save<T>(val: T): string;
@@ -11,7 +12,36 @@ interface SQLQueryConstructorMethods {
  * ! Use for create SQL queries dinamically
  * * whitehatdevv - 2021/12/21
  */
-class SQLQueryConstructor {
+class SQLQueryConstructor implements SQLQueryConstructorMethods {
+  // * Properties
+  _tableName = '@table';
+
   // * Singleton
   static shared = new SQLQueryConstructor();
+
+  // * Methods
+  findAll(get?: [string]): string {
+    return "";
+  }
+
+  findBy(get?: [string], where?: object): string {
+    return "";
+  }
+
+  save<T>(val: T): string {
+    return "";
+  } 
+
+  update<T>(where?: object, data?: T): string {
+      return "";
+  }  
+
+  deleteAll(): string {
+    return "";
+  }
+
+  delete(where?: object): string {
+    return "";
+  } 
+
 }
