817a897cf60dea7abb08d936198789734d5104a5
Emanuel Martinez <me@manumartinez.es>
2021-12-21 08:02:54 +0100

    Create basic classes

--- src/http/core/build/core.build.sql.ts
+++ src/http/core/build/core.build.sql.ts
@@ -1,1 +1,30 @@
-//TODO: IMPLEMENT IT
+interface SQLQueryMethods {
+    _tableName: string;
+    findAll<T>() : T;
+}
+
+/**
+ * ! Use for create SQL queries dinamically
+ * * whitehatdevv - 2021/12/21
+ */
+class SQLQuery {
+
+    /**
+     * ! SINGLETON
+     * * Unique access to this resource
+     */
+    static shared = new SQLQuery();
+
+    // * Properties
+    _tableName: string;
+
+    // * Inits
+    constructor(tableName: string) {
+        this._tableName = tableName;
+    }
+
+    // * Methods
+
+
+
+}
