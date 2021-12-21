d0725c7c5403962ef4a373ea32ac71bd6f461002
Emanuel Martinez <me@manumartinez.es>
2021-12-21 14:25:56 +0100

    Create basic table structure

--- src/http/core/build/core.build.sql.ts
+++ src/http/core/build/core.build.sql.ts
@@ -1,5 +1,4 @@
 interface SQLQueryConstructorMethods {
-  _tableName: string;
   findAll(get?: [string]): string;
   findBy(where: object, get?: [string]): string;
   save<T>(val: T): string;
@@ -8,33 +7,31 @@ interface SQLQueryConstructorMethods {
   delete(where?: object): string;
 }
 
+const _basicSQLCodes = {
+    table: '@table',
+    columns: '@column',
+    condition: '@condition'
+};
+
+const _basicSQLStructure = {
+    select: 'SELECT @columns FROM @table WHERE @condition',
+    insert: 'INSERT INTO @table VALUES @columns',
+    update: 'UPDATE @table SET @columns',
+    delete: 'DELETE FROM @table WHERE @condition'
+};
+
 /**
  * ! Use for create SQL queries dinamically
  * * whitehatdevv - 2021/12/21
  */
 class SQLQueryConstructor implements SQLQueryConstructorMethods {
-  // * Properties
-  _tableName = '@table';
 
   // * Singleton
   static shared = new SQLQueryConstructor();
 
   // * Methods
-
-  private getter(get: [string]): string {
-    const columns = get.reduce((acc, val) => (acc += `${val}#`));
-    columns.slice(0, columns.length - 2);
-    columns.replace('#', ',');
-    return columns;
-  }
-
   findAll(get?: [string]): string {
-    const query = `SELECT * FROM ${this._tableName}`;
-    if (!get) {
-      const getter = this.getter(get);
-      query.replace('*', getter);
-    }
-    return query;
+      return "";
   }
 
   findBy(where: object, get?: [string]): string {
