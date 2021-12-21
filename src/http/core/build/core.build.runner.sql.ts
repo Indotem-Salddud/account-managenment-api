9e1dc92fc0fbb5f021420ef6dbf04758181bb898
Emanuel Martinez <me@manumartinez.es>
2021-12-21 12:00:09 +0100

    Documented query runner

--- src/http/core/build/core.build.runner.sql.ts
+++ src/http/core/build/core.build.runner.sql.ts
@@ -1,19 +1,30 @@
 import mysql from 'mysql';
 import {db} from '../core.db';
 
+// * Interface used for query runner callback
 interface SQLQueryResponse<T> {
   err?: string;
   data?: T;
 }
 
+// * Used for make a callback with no return
 type SQLResponse<T> = (res: SQLQueryResponse<T>) => void;
 
 interface SQLNeededInjections {
+  // * Table name to run all the staff needed
   _tableName: string;
 }
 
 interface SQLRunnerProtocol extends SQLNeededInjections {
+  // * It will be used to inject the DB connection Pool
   _db: mysql.Connection;
+  /**
+   * ! Query runner allow us to run query in sql
+   * * whitehatdevv - 2021/12/21
+   * @param query {string}
+   * @param params {any}
+   * @param callback {SQLResponse<T>}
+   */
   run<T>(query: string, params: any, callback: SQLResponse<T>);
 }
 
@@ -30,7 +41,9 @@ class SQLRunner implements SQLRunnerProtocol {
 
   // * Methods
   run<T>(query: string, params: any, callback: SQLResponse<T>) {
-    db.query(query, params, (err, data) => {
+    // replace query concurrency to set table name
+    const final = query.replace('@table', this._tableName);
+    db.query(final, params, (err, data) => {
       callback({
         err: String(err),
         data: data,
