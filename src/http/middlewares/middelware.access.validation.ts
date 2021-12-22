2d2676174ce3853ec0901091969237498a7f79f5
Manu Martinez <me@manumartinez.es>
2021-12-22 09:30:58 +0100

    FEATURE: Access granted permission validator (Middelware)
    * Setted basic structure
    * Create initial structure of the middelware
    * Create function to run validations
    * Refactor function
    * Documented function
    * TS Format applied
    * Reorganize files

---
+++ src/http/middlewares/middelware.access.validation.ts
@@ -0,0 +1,30 @@
+import {Query, Permission} from 'accesscontrol';
+import express from 'express';
+import {ac} from '../../models/types/gen/gen.permissions';
+import {_handleResponse} from '../common/common.responseHandler';
+
+export module AccessControlMiddelware {
+  /**
+   * ! Create a validator using Lambda functions
+   * * whitehatdevv - 2021/12/20
+   * @param f lambda(query: Query) => Permission
+   * @returns {MiddelwareObject}
+   */
+  export const _grantAccess = (f: (query: Query) => Permission) => {
+    return (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      // make a lambda function to run a validation
+      const permission = f(ac.can(req.user.role));
+      if (!permission.granted) {
+        _handleResponse(
+          {statusCode: 500, message: 'Server response cannot be processed'},
+          res
+        );
+      }
+      next();
+    };
+  };
+}
