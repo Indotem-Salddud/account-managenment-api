fd0e991070fd8dadbb6cbe4c4a9f88f61300fab8
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2021-12-29 21:53:24 +0100

    New dependents DTOs

--- src/models/types/model.dependent.ts
+++ src/models/types/model.dependent.ts
@@ -2,10 +2,22 @@ import {Identificable} from './gen/gen.identificable';
 import {Direction} from './gen/gen.direction';
 
 export const _dependentTableName = 'Dependents';
+
 export interface Dependent extends Identificable {
   name: string;
   phone: string;
   direction: Direction;
   status: number;
   date: Date;
+}
+
+export interface newDependentDTO {
+  name: string;
+  phone?: string;
+  direction?: string;
+}
+
+export interface newDependentForCustomerDTO {
+  customersIds: Array<string>;
+  dependent: newDependentDTO;
 }
