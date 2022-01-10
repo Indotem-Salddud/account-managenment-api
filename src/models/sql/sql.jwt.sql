cee8e8cb08ae240937c7b787ed206bcbe8d81681
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2022-01-10 17:12:35 +0100

    Remove refresh token db model and actions

--- src/models/sql/sql.jwt.sql
+++
@@ -1,9 +0,0 @@
--- Contains JWT refresh tokens
-CREATE TABLE `JWTRefreshTokens` IF NOT EXISTS (
-    customerID INT PRIMARY KEY NOT NULL,
-    refreshToken BINARY(32), /*TODO: is 256 bytes ok?*/
-    expiration TIMESTAMP,
-    FOREIGN KEY (customerID) REFERENCE customers(id),
-) ENGINE = InnoDB,
-CHARSET = utf8,
-COLLATE = utf8_spanish2_ci;
