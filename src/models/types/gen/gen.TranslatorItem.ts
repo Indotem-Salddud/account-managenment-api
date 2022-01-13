e80fbc1089d1ad76f4d64a634815cd8f31d31aad
Emanuel Martinez <me@manumartinez.es>
2022-01-13 07:30:20 +0100

    Create translator key interface

---
+++ src/models/types/gen/gen.TranslatorItem.ts
@@ -0,0 +1,10 @@
+/**
+ * ! Translator Item to call directly
+ * * whitehatdevv - 2022/01/12
+ */
+export interface TranslatorItem {
+    // * Key provided by the excel file
+    key: string;
+    // * UUID provided by the excel file
+    uuid: string;
+}
