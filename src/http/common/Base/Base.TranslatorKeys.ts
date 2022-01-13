e53dc8a94380700ef0133dc708ee88a7d4332702
Emanuel Martinez <me@manumartinez.es>
2022-01-13 07:36:59 +0100

    Created basic translator keys

---
+++ src/http/common/Base/Base.TranslatorKeys.ts
@@ -0,0 +1,15 @@
+/**
+ * ! Translator keys collection
+ * * whitehatdevv - 2022/01/12
+ */
+export enum TranslatorKeys {
+    AppAuthLoginBadRequest = 'app_auth_login_post_bad_request'
+}
+
+/**
+ * ! TranslatorKeysUUID collection
+ * * whitehatdevv - 2022/01/12
+ */
+export enum TranslatorKeysUUID {
+    AppAuthLoginBadRequest = 'e1a6cdf1-a618-4f3c-bc81-3850ce17d4b9'
+}
