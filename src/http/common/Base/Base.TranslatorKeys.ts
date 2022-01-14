8d0eecbdbe4c83943edae287cd93c97721c392c2
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-01-14 12:56:47 +0100

    Add new keys and UUIDs

--- src/http/common/Base/Base.TranslatorKeys.ts
+++ src/http/common/Base/Base.TranslatorKeys.ts
@@ -2,8 +2,10 @@
  * ! Translator keys collection
  * * whitehatdevv - 2022/01/12
  */
-export enum TranslatorKeys { // ggignore
-    AppAuthLoginBadRequest = 'app_auth_login_post_bad_request'
+export enum TranslatorKeys {
+    AppAuthLoginBadRequest = 'app_auth_login_post_bad_request',
+    AppAuthCustomersCustomerIDDeleteErrorRemovingUnrelatedDependents = 'app_auth_customers_customerID_delete_error_removing_unrelated_dependents',
+    AppAuthCustomersCustomerIDDeleteSuccessfully= 'app_auth_customers_customerID_delete_successfully'
 }
 
 /**
@@ -11,5 +13,7 @@ export enum TranslatorKeys { // ggignore
  * * whitehatdevv - 2022/01/12
  */
 export enum TranslatorKeysUUID { // ggignore
-    AppAuthLoginBadRequest = 'e1a6cdf1-a618-4f3c-bc81-3850ce17d4b9' // ggignore
+    AppAuthLoginBadRequest = 'e1a6cdf1-a618-4f3c-bc81-3850ce17d4b9', // ggignore
+    AppAuthCustomersCustomerIDDeleteErrorRemovingUnrelatedDependents = 'ff4fb602-6b2f-4c73-988a-4d45ce714cb2',
+    AppAuthCustomersCustomerIDDeleteSuccessfully = '1f70fcd8-002f-416d-bef3-9d512012e54d'
 }
