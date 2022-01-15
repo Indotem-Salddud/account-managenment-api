8f56ac910709ced8234c0b3c8f298e4c4a6dca3e
Emanuel Martinez <me@manumartinez.es>
2022-01-15 13:21:15 +0100

    Added verification middelware to all needed endpoints

--- src/routes/routes.auth.ts
+++ src/routes/routes.auth.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import {AuthController} from '../http/controllers/controller.auth';
-import { PermissionActions } from '../models/types/gen/gen.permissions';
-import { AccessControlMiddelware } from '../http/middlewares/middelware.access.validation';
-import { AuthEndpoints } from '../http/common/Base/Base.AuthEndpoints';
+import {PermissionActions} from '../models/types/gen/gen.permissions';
+import {AccessControlMiddelware} from '../http/middlewares/middelware.access.validation';
+import {AuthEndpoints} from '../http/common/Base/Base.AuthEndpoints';
+import {JWTMiddelware} from '../http/middlewares/middelware.jwt';
 
 // * Global variables
 const _resource = PermissionActions.CUSTOMER;
 
 export const CustomersRoute = (app: express.Application) => {
-
   /**
    * * Login
    * @body {
@@ -17,14 +17,21 @@ export const CustomersRoute = (app: express.Application) => {
    * }
    */
   app.post(AuthEndpoints.Login, AuthController._login);
-   /**
+  /**
    * * Update customer password
    * @protected Only customer with ID provided by Token
    * @body {
-   *  @param password {string} 
+   *  @param password {string}
    * }
    */
-  app.put(AuthEndpoints.UpdatePassword, AccessControlMiddelware._grantAccess((query) => {
-    return query.updateOwn(_resource);
-  }), AuthController._updatePassword);
+  app.put(
+    AuthEndpoints.UpdatePassword,
+    [
+      JWTMiddelware._verify,
+      AccessControlMiddelware._grantAccess(query => {
+        return query.updateOwn(_resource);
+      }),
+    ],
+    AuthController._updatePassword
+  );
 };
