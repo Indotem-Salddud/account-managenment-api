60262c6814add3c91feac746560344c7e4730b2b
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-01-18 13:09:43 +0100

    Add error handler and translatios to auth controller

--- src/http/controllers/controller.auth.ts
+++ src/http/controllers/controller.auth.ts
@@ -2,6 +2,7 @@ import {AuthActions} from '../actions/actions.auth';
 import { AuthEndpoints } from '../common/Base/Base.AuthEndpoints';
 import {s} from '../common/common.responseHandler';
 import {JWTMiddelware} from '../middlewares/middelware.jwt';
+import { error } from '../common/common.handlerGenerator';
 
 // * Global properties
 const _microservice = 'Auth';
@@ -25,9 +26,20 @@ export module AuthController {
         if (err || !ctrl) {
           s(
             500,
-            {
-              message: err,
-            },
+            error(
+              [
+                {
+                  message: "app_auth_login_post_internal_server_error",
+                  code: "90fcdcb0-c8ca-4a95-a586-4178292a59cd",
+                  date: _date
+                }
+              ],
+              {
+                endpoint: AuthEndpoints.Login,
+                microservice: _microservice,
+                version: _version
+              }
+            ),
             res
           );
         }
@@ -36,7 +48,7 @@ export module AuthController {
         s(
           200,
           {
-            message: 'Login sucesfully',
+            message: 'app_auth_login_post_login_sucesfully',
           },
           res
         );
@@ -58,16 +70,27 @@ export module AuthController {
       if (err) {
         s(
           500,
-          {
-            message: 'Password cannot be updated',
-          },
+          error(
+            [
+              {
+                message: "app_auth_password_put_internal_server_error",
+                code: "0a3adaec-9f5d-4f13-aed6-fc94ce79dfd9",
+                date: _date
+              }
+            ],
+            {
+              endpoint: AuthEndpoints.Login,
+              microservice: _microservice,
+              version: _version
+            }
+          ),
           res
         );
       }
       s(
         200,
         {
-          message: 'Password was updated sucessfully',
+          message: 'app_auth_password_put_updated_sucessfully',
         },
         res
       );
