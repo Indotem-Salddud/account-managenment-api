73e2ef5960fe851545d2bb7dae9b3855901a034a
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2022-01-20 13:18:07 +0100

    Adds refhes token trable name

--- src/models/types/model.token.ts
+++ src/models/types/model.token.ts
@@ -2,6 +2,9 @@
  * ! Token interface use for pass user the token data
  * * whitehatdevv - 2021/12/12
  */
+
+export const _tokenTableName = "JWTRefreshTokens";
+
 export interface Token {
   token: string;
   expiration: Number;
